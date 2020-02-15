d4ea582adb86c48f720b76a2a11df91cea335915
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2020-02-16 02:06:36 +0530

    Preview TT functionality (#1)
    * Add initial preview to timetable
    
    * Make App stateful
    
    * Add functionality for multiple hours displayed as a single block
    
    * Add header rows/ columns, centre text
    
    * Remove unnecessary code, fine tune

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,98 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 import Search from "./components/Search";
+import Timetable from "./components/TimeTable";
 
-function App() {
-  const courses = [
-    {
-      code: "CS F211",
-      name: "DSA",
-      lecture: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      tutorial: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      lab: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      midsem: "3 Mar",
-      compre: "4 May"
-    },
-    {
-      code: "CS F212",
-      name: "OOPS",
-      lecture: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      tutorial: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      lab: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      midsem: "3 Mar",
-      compre: "4 May"
+
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      isRoot: 1,
+      isPreview: 0
+
     }
-  ];
+    this.courses = [
+      {
+        code: "CS F211",
+        name: "DSA",
+        lecture: [
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+        ],
+        tutorial: [
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+        ],
+        lab: [
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+        ],
+        midsem: "3 Mar",
+        compre: "4 May"
+      },
+      {
+        code: "CS F212",
+        name: "OOPS",
+        lecture: [
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+        ],
+        tutorial: [
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+        ],
+        lab: [
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+          { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+        ],
+        midsem: "3 Mar",
+        compre: "4 May"
+      }
+        ];
+      this.sendPrevOrBack = this.sendPrevOrBack.bind(this);
 
-  return (
-    <div className="App">
-      <Search items={courses} />
-    </div>
-  );
+  }
+  sendPrevOrBack()
+  {
+    this.setState(state => ({
+      isPreview: !state.isPreview
+    }));
+    this.setState(state =>({
+      isRoot: !state.isRoot
+    }));
+  }
+  render()
+  {
+    if(this.state.isPreview){
+      return(
+        <>
+        <div>
+          <button type="Button" onClick = {this.sendPrevOrBack}>Back</button>
+        </div>
+        <div>
+          <Timetable />
+        </div>
+        </>
+      );
+
+    }
+    else if(this.state.isRoot)
+    {
+      return (
+        <>
+        <div>
+          <button type="Button" onClick = {this.sendPrevOrBack}>View Preview</button>
+        </div>
+        <div className="App">
+          <Search items={this.courses} />
+        </div>
+        </>
+
+      );
+    }
+  }
 }
 
 export default App;
---
+++ client/src/components/TimeTable.jsx
@@ -0,0 +1,201 @@
+import { Component } from "react";
+import React from "react";
+import "../styles/Timetable.css";
+class Timetable extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            courses: [
+                
+                {
+                    Name: "DBMS",
+                    ID: "CS F212",
+                    Days: "Monday Wednesday Friday",
+                    Hours:"5",
+                    Section: "L1"
+                },
+                {
+                    Name: "MicroProcessors",
+                    ID: "CS F241",
+                    Days: "Monday Wednesday Friday",
+                    Hours:"4",
+                    Section: "L1"
+                },
+                {
+                    Name: "Software Engineering",
+                    ID: "IS F341",
+                    Days: "Monday Wednesday Friday",
+                    Hours:"7",
+                    Section: "L1"
+                },
+                {
+                    Name: "EVS",
+                    ID: "BITS F225",
+                    Days: "Tuesday Thursday Saturday",
+                    Hours:"2",
+                    Section: "L1"
+                },
+                {
+                    Name: "Post Colonial Literature",
+                    ID: "HSS F340",
+                    Days: "Tuesday Thursday Saturday",
+                    Hours:"3",
+                    Section: "L1"
+                },
+                {
+                    Name: "MicroProcessors",
+                    ID: "CS F241",
+                    Days: "Tuesday",
+                    Hours:"1",
+                    Section: "T11"
+                },
+                {
+                    Name: "DSA",
+                    ID: "CS F211",
+                    Days: "Thursday",
+                    Hours:"1",
+                    Section: "T3"
+                },
+                
+                {
+                    Name: "DSA",
+                    ID: "CS F211",
+                    Days: "Tuesday Thursday Saturday",
+                    Hours:"5 6 7 8",
+                    Section: "L1"
+                },
+                
+            ]
+        }
+        this.populateTimetable.bind(this);
+        this.gridArray = this.populateTimetable(this.state.courses); 
+    }
+    populateTimetable(coursesAdded)
+    {
+        var gridList = [
+            ["Time",1,2,3,4,5,6,7,8,9,10],
+            ["Monday",0,0,0,0,0,0,0,0,0,0],
+            ["Tuesday",0,0,0,0,0,0,0,0,0,0],
+            ["Wednesday",0,0,0,0,0,0,0,0,0,0],
+            ["Thursday",0,0,0,0,0,0,0,0,0,0],
+            ["Friday",0,0,0,0,0,0,0,0,0,0],
+            ["Saturday",0,0,0,0,0,0,0,0,0,0]
+        ];
+       
+        var Map = {};
+         Map['Monday'] = 1;
+         //Map[1] = 'Monday';
+         Map['Tuesday'] = 2;
+         //Map[2] = 'Tuesday';
+         Map['Wednesday'] = 3;
+         //Map[3] = 'Wednesday';
+         Map['Thursday'] = 4;
+         //Map[4] = 'Thursday';
+         Map['Friday'] = 5;
+         //Map[5] = 'Friday';
+         Map['Saturday'] = 6;
+         //Map[6] = 'Saturday';
+            
+            //let each course have a day list, and a hours list, both space separated strings
+            for(var course of coursesAdded)
+            {    
+                
+                var listOfDays = Array.from(course.Days.split(' '));
+                var listOfHours = (Array.from(course.Hours.split(' ')));
+                for(var i = 0; i< listOfHours.length; i++)
+                {
+                    listOfHours[i] = +listOfHours[i];
+                }
+                for(i of listOfDays)
+                {
+                    var divStyle = {};
+                    if(listOfHours.length > 1)
+                    {
+                         divStyle = {
+                           gridRowStart: `${listOfHours[0]+1}`,
+                           gridColumnStart: `${Map[i]+1}`,
+                           gridCoulmnEnd: `${Map[i]+2}`,
+                           gridRowEnd: `span ${listOfHours[listOfHours.length-1]-listOfHours[0]+1}`
+                        }
+                    }
+                    let str = <>
+                                <div className="gridItem" style={divStyle}>
+                                    {course.Name}<br></br>
+                                    {course.ID}<br></br>
+                                    {course.Section}<br></br>
+                                </div>
+                            </>;
+                    
+                    gridList[Map[i]][listOfHours[0]] = str;
+                    for(var j = listOfHours[0]+1 ; j < listOfHours[0]+listOfHours.length;j++)
+                    {
+                        gridList[Map[i]][j] = -1;
+                    }
+
+                }
+            }
+
+            for(i = 0; i<=6; i++)
+                for(j =0 ; j<= 10; j++)
+                {
+                    if(gridList[i][j] === 0)
+                    {
+                        gridList[i][j] =
+                         <>
+                            <div style={{
+                                        backgroundColor: '#ffffff',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center'
+                                        }}>
+                                {' '}<br></br>
+                                {' '}<br></br>
+                                {' '}<br></br>
+                            </div>
+                        </>;  
+                    }
+                    else if(j === 0 || i === 0)
+                    {
+                        gridList[i][j] = 
+                        <>
+                            <div style={{
+                                        backgroundColor: "#d6afc7",
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center'
+                                }}>
+                                {gridList[i][j]}
+                            </div>
+                        </>;
+                    }
+                }
+            
+            
+            return gridList;
+    }
+    render()
+    {
+       var divsToRender = [];
+       for(var i = 0; i<= 10;i++)
+       {
+           for(var j = 0; j<=6; j++)
+           {
+               if(this.gridArray[j][i] === -1);
+               else{
+               divsToRender.push(
+                    <>
+                        {this.gridArray[j][i]}
+                    </>
+                    );
+               }
+            }
+        }
+        return (
+            <div className = "gridElement" >
+                {divsToRender}
+            </div>
+        );
+    }
+}
+export default Timetable;
