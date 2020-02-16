69c3ef1ca69ea24925465c4b6063aa929952ba79
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2020-02-16 16:59:07 +0530

    Revert "Merge branch 'feature' into dev"
    This reverts commit 15e8bdd525b6dbcb41217564802c58a4379e2f3a, reversing
    changes made to d4ea582adb86c48f720b76a2a11df91cea335915.

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,8 @@ class App extends Component {
   {
     super(props);
     this.state = {
-<<<<<<< HEAD
       isRoot: 1,
       isPreview: 0
-=======
-      isRoot: true,
-      isPreview: false
->>>>>>> feature
 
     }
     this.courses = [
@@ -56,7 +51,6 @@ class App extends Component {
         compre: "4 May"
       }
         ];
-<<<<<<< HEAD
       this.sendPrevOrBack = this.sendPrevOrBack.bind(this);
 
   }
@@ -68,26 +62,6 @@ class App extends Component {
     this.setState(state =>({
       isRoot: !state.isRoot
     }));
-=======
-      
-      this.view = {ROOT: 0, PREVIEW: 1};
-  }
-  sendTo(view)
-  {
-    if(view === this.view.ROOT){
-    this.setState(state => ({
-      isPreview: false,
-      isRoot: true
-    }));
-   }
-   else if(view === this.view.PREVIEW)
-   {
-     this.setState(state => ({
-       isPreview: true,
-       isRoot: false
-     }))
-   }
->>>>>>> feature
   }
   render()
   {
@@ -95,11 +69,7 @@ class App extends Component {
       return(
         <>
         <div>
-<<<<<<< HEAD
           <button type="Button" onClick = {this.sendPrevOrBack}>Back</button>
-=======
-          <button type="Button" onClick = {this.sendTo.bind(this,this.view.ROOT)}>Back</button>
->>>>>>> feature
         </div>
         <div>
           <Timetable />
@@ -113,11 +83,7 @@ class App extends Component {
       return (
         <>
         <div>
-<<<<<<< HEAD
           <button type="Button" onClick = {this.sendPrevOrBack}>View Preview</button>
-=======
-          <button type="Button" onClick = {this.sendTo.bind(this,this.view.PREVIEW)}>View Preview</button>
->>>>>>> feature
         </div>
         <div className="App">
           <Search items={this.courses} />
