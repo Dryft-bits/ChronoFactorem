8f892e2a98c0cca9963e50c1780c2aea173a58de
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-02-17 12:36:52 +0530

    Convert App into a stateful component

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,67 @@
-import React from "react";
-import "./App.css";
-import Search from "./components/Search";
-import Catalog from "./components/Catalog";
-import "./styles/catalog.css";
+import React, { Component } from "react";
+//import TimeTable from "./schemas/TimeTable.js";
+import AddCourse from "./components/AddCourse.jsx";
 
-function App() {
-  const courses = [
-    {
-      code: "CS F211",
-      name: "DSA",
-      lecture: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      tutorial: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      lab: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      midsem: "3 Mar",
-      compre: "4 May"
-    },
-    {
-      code: "CS F212",
-      name: "OOPS",
-      lecture: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      tutorial: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      lab: [
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
-        { id: 1, teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
-      ],
-      midsem: "3 Mar",
-      compre: "4 May"
-    }
-  ];
-
-  return (
-    <div className="App">
-      <Search items={courses} />
-      <Catalog courses={courses} />
-    </div>
-  );
+//var TimeTableData = require("./Timetable.json");
+//const courses = JSON.parse(TimeTableData);
+const courses = [
+  {
+    code: "CS F211",
+    name: "DSA",
+    lectures: [
+      {
+        code: "L1",
+        teacher: "BHANUMURTHY",
+        hours: " 6 7",
+        room: "ht",
+        days: "hk"
+      },
+      { code: "L2", teacher: "MITRA", hours: " 6 7", room: "ht", days: "hk" }
+    ],
+    tutorials: [
+      { code: "T1", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+      { code: "T2", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+    ],
+    labs: [
+      { code: "P1", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+      { code: "P2", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+    ],
+    mcodesem: "3 Mar",
+    compre: "4 May"
+  },
+  {
+    code: "CS F212",
+    name: "OOPS",
+    lectures: [
+      { code: "L1", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+      { code: "L2", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+    ],
+    tutorials: [
+      { code: "T1", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+      { code: "T2", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+    ],
+    labs: [
+      { code: "P1", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" },
+      { code: "P2", teacher: "abc", hours: " 6 7", room: "ht", days: "hk" }
+    ],
+    midsem: "3 Mar",
+    compre: "4 May"
+  }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { myTimeTable: [], myCourses: [] };
+  }
+  render() {
+    return (
+      <AddCourse
+        allCourses={courses}
+        TimeTable={this.state.selected}
+        myCourses={this.state.myCourses}
+      />
+    );
+  }
 }
 
 export default App;
