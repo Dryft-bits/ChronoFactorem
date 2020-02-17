7aaa75ee3b7a205908e7f22bad0f1b75e5ee3cd3
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-02-17 15:44:51 +0530

    Create WeekDay Schema

---
+++ client/src/schemas/WeekDay.js
@@ -0,0 +1,16 @@
+class WeekDay {
+  constructor() {
+    this.hour1 = null;
+    this.hour2 = null;
+    this.hour3 = null;
+    this.hour4 = null;
+    this.hour5 = null;
+    this.hour6 = null;
+    this.hour7 = null;
+    this.hour8 = null;
+    this.hour9 = null;
+    this.hour10 = null;
+  }
+}
+
+export default WeekDay;
