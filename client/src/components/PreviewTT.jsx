a3439c6ef6668229c02a4a8eb5d2c4738c0e8044
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2020-02-20 23:33:46 +0530

    Refactor code

--- client/src/components/PreviewTT.jsx
+++ client/src/components/PreviewTT.jsx
@@ -7,7 +7,7 @@ class PreviewTT extends Component{
     constructor(props)
     {
         super(props);  
-        //console.log(this.props.TimeTable);
+        
         this.populateTimetable.bind(this);
         this.gridArray = []; 
     }
@@ -23,188 +23,86 @@ class PreviewTT extends Component{
             ["Saturday",0,0,0,0,0,0,0,0,0,0]
         ];
        
-        var Map = {};
-         Map['M'] =1;
-         //Map[1] = 'Monday';
-         Map['T'] = 2;
-         //Map[2] = 'Tuesday';
-         Map['W'] = 3;
-         //Map[3] = 'Wednesday';
-         Map['Th'] = 4;
-         //Map[4] = 'Thursday';
-         Map['F'] = 5;
-         //Map[5] = 'Friday';
-         Map['S'] = 6;
-         //Map[6] = 'Saturday';
+        let Map = {};
+        Map['M'] =1;
+        Map['T'] = 2;
+        Map['W'] = 3;
+        Map['Th'] = 4;
+        Map['F'] = 5;
+        Map['S'] = 6;
+         
+        let divStyle = {};
+        var days=['M','T','W','Th','F','S'];
+        var hours= [1,2,3,4,5,6,7,8,9,10];
+        var day,hour;
+        for(day of days){
+            for(hour of hours){
+                divStyle = {};
+                var section=this.props.TimeTable[day][ntw.toWords(hour)];
+                console.log(section);
+                let str = "";
+                if(gridList[Map[day]][hour] === -1)
+                {
+                    continue;
+                }
+                else if(!section.courseCode)
+                {
+                   str = <div style={{
+                        backgroundColor: '#ffffff',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                        }}>
+                        {' '}<br></br>
+                        {' '}<br></br>
+                        {' '}<br></br>
+                    </div>
+                }
+                else
+                { 
+                    if(section.numHours > 1)
+                    {
+                        for(let i = 1; i< section.numHours; i++)
+                        {
+                            gridList[Map[day]][hour+i] = -1;
+                        }
+                        divStyle = {
+                            gridRowStart: `${hour+1}`,
+                            gridColumnStart: `${Map[day]+1}`,
+                            gridCoulmnEnd: `${Map[day]+2}`,
+                            gridRowEnd: `span ${section.numHours}`
+                         }
+                    }
+                    
+                str =
+                <div className="gridItem" style={divStyle}>
+                    {section.courseName}<br></br>
+                    {section.courseCode}<br></br>
+                    {section.sectionRoom}<br></br>
+                </div>;
+                }
             
-            //let each course have a day list, and a hours list, both space separated strings
-            /*
-            for(var course of coursesAdded)
-            {    
+                gridList[Map[day]][hour] = str;
+            }
+        }
+        for(let i = 0; i <=6; i++)
+            for(let j = 0; j <=10;j++)
+            {
+             if(j === 0 || i === 0)
+             {
+                gridList[i][j] = 
                 
-                var listOfDays = this.props.TimeTable;
-                
-                var listOfHours = (Array.from(course.Hours.split(' ')));
-                for(var i = 0; i< listOfHours.length; i++)
-                {
-                    listOfHours[i] = +listOfHours[i];
-                }
-                
-               
-               
-                for(let i of listOfDays)
-                {
-                   
-                    
-                    for(let j of i)
-                    {
-                        counter++;
-                        if(j !== null)
-                        {
-                            listOfHours.push(counter);
-                        }
-                    }
-                    if(listOfHours.length > 1)
-                    {
-                         divStyle = {
-                           gridRowStart: `${listOfHours[0]+1}`,
-                           gridColumnStart: `${Map[i]+1}`,
-                           gridCoulmnEnd: `${Map[i]+2}`,
-                           gridRowEnd: `span ${listOfHours[listOfHours.length-1]-listOfHours[0]+1}`
-                        }
-                    }
-                    let str = <>
-                                <div className="gridItem" style={divStyle}>
-                                    {course.Name}<br></br>
-                                    {course.ID}<br></br>
-                                    {course.Section}<br></br>
-                                </div>
-                            </>;
-                    
-                   
-                    //gridList[][listOfHours[0]] = str;
-                    
-                    for(var j = listOfHours[0]+1 ; j < listOfHours[0]+listOfHours.length;j++)
-                    {
-                        gridList[Map[i]][j] = -1;
-                    }
-                    
-
-                }
-                */
-               let divStyle = {};
-               //let listOfHours = [];
-               //let counter = 0;
-                var days=['M','T','W','Th','F','S'];
-                var hours= [1,2,3,4,5,6,7,8,9,10];
-                var day,hour;
-                for(day of days){
-                    for(hour of hours){
-                        divStyle = {};
-                        var section=this.props.TimeTable[day][ntw.toWords(hour)];
-                        console.log(section);
-                        let str = "";
-                        if(gridList[Map[day]][hour] === -1)
-                        {
-                            continue;
-                        }
-                        else if(!section.courseCode)
-                        {
-                           str = <div style={{
-                                backgroundColor: '#ffffff',
+                    <div style={{
+                                backgroundColor: "#d6afc7",
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center'
-                                }}>
-                                {' '}<br></br>
-                                {' '}<br></br>
-                                {' '}<br></br>
-                            </div>
-                        }
-                        else
-                        {
-                            
-                            if(section.numHours > 1)
-                            {
-                                for(let i = 1; i< section.numHours; i++)
-                                {
-                                    gridList[Map[day]][hour+i] = -1;
-                                }
-                                divStyle = {
-                                    gridRowStart: `${hour+1}`,
-                                    gridColumnStart: `${Map[day]+1}`,
-                                    gridCoulmnEnd: `${Map[day]+2}`,
-                                    gridRowEnd: `span ${section.numHours}`
-                                 }
-                            }
-                            
-                        str =
-                        <div className="gridItem" style={divStyle}>
-                            {section.courseName}<br></br>
-                            {section.courseCode}<br></br>
-                            {section.sectionRoom}<br></br>
-                        </div>;
-                        }
-                    
-                        gridList[Map[day]][hour] = str;
-                    }
+                        }}>
+                        {gridList[i][j]}
+                    </div>
+                
                 }
-                for(let i = 0; i <=6; i++)
-                    for(let j = 0; j <=10;j++)
-                    {
-                     if(j === 0 || i === 0)
-                     {
-                        gridList[i][j] = 
-                        <>
-                            <div style={{
-                                        backgroundColor: "#d6afc7",
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                }}>
-                                {gridList[i][j]}
-                            </div>
-                        </>;
-                        }
-                    }
-
-            /*
-            for(let i = 0; i<=6; i++)
-                for(let j =0 ; j<= 10; j++)
-                {
-                    if(gridList[i][j] === 0)
-                    {
-                        gridList[i][j] =
-                         <>
-                            <div style={{
-                                        backgroundColor: '#ffffff',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                        }}>
-                                {' '}<br></br>
-                                {' '}<br></br>
-                                {' '}<br></br>
-                            </div>
-                        </>;  
-                    }
-                    else if(j === 0 || i === 0)
-                    {
-                        gridList[i][j] = 
-                        <>
-                            <div style={{
-                                        backgroundColor: "#d6afc7",
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                }}>
-                                {gridList[i][j]}
-                            </div>
-                        </>;
-                    }
-                }
-            
-            */
+            }
             return gridList;
     }
     render()
