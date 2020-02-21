d20c86aa64607ef9b874408e8b8e8f29ae88ada7
HarshvardhanJha1 <harshvardhanjha1@gmail.com>
2020-02-22 00:17:30 +0530

    Change Navbar Design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import "../styles/Navbar.css";
 const Navbar = (props) => {
     return ( <div>
-        <nav className="nav-wraper indigo">
+        <nav className="nav-wraper blue">
             <div className="container">
-            <option className="brand-logo">Site Title</option>
+            <a href="#" class="brand-logo left">ChronoFactorem</a>
             <ul className="right hide-on-med-and-down" onClick={props.action}>
-                <option className="waves-effect waves-light btn">Dashboard</option>
-                <option className="waves-effect waves-light btn">Create TimeTable</option>
-                <option className="waves-effect waves-light btn">Share TimeTable</option>
-                <option className="waves-effect waves-light btn">About Us</option>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Create TimeTable</a></li>
+                <li><a href="#">Share TimeTable</a></li>
+                <li><a href="#">About Us</a></li>
             </ul>
             </div>
         </nav>
