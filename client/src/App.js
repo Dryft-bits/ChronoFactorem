ece27fd40211939897616f1c9185f0e64d31b7d6
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-02-23 16:35:46 +0530

    Revert "Implement google auth"

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,43 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Dashboard from "./components/layout/Dashboard";
-import CreateTimeTable from "./components/timetable/CreateTimeTable";
-import ShareTimeTable from "./components/timetable/ShareTimeTable";
-import AboutUs from "./components/layout/AboutUs";
-import Landing from "./components/layout/Landing";
-import axios from "axios";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import CreateTimeTable from "./components/CreateTimeTable";
+import ShareTimeTable from "./components/ShareTimeTable";
+import AboutUs from "./components/AboutUs";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentTab: "Create TimeTable", isLoggedin: false };
+    this.state = { currentTab: "Create TimeTable" };
     this.changeTab = this.changeTab.bind(this);
-    axios.get("/loggedin").then(res => {
-      console.log(res);
-      if (res.status === 200) {
-        this.setState({ isLoggedin: true });
-      }
-    });
   }
-
-  userLoggedIn() {
-    axios.get("/loggedin").then(res => {
-      console.log(res);
-      if (res.status === 200) {
-        this.setState({ isLoggedin: true });
-      }
-    });
-  }
-
-  toggleLoggedin() {
-    this.setState({ ...this.state, isLoggedin: false });
-  }
-
   changeTab(input) {
     console.log(input.target.text);
     let selectedTab = input.target.text;
-    if (input.target.text === "Logout" && this.state.isLoggedin) {
-      this.setState({
-        currentTab: selectedTab,
-        isLoggedin: false
-      });
-    } else {
-      this.setState({
-        currentTab: selectedTab
-      });
-    }
+    this.setState({
+      currentTab: selectedTab
+    });
   }
   render() {
-    if (!this.state.isLoggedin) {
-      return (
+    return (
+      <div>
+        <Navbar action={this.changeTab} />
         <div>
-          <Landing></Landing>
+          {this.state.currentTab === "Dashboard" ? (
+            <Dashboard />
+          ) : this.state.currentTab === "Create TimeTable" ? (
+            <CreateTimeTable />
+          ) : this.state.currentTab === "Share TimeTable" ? (
+            <ShareTimeTable />
+          ) : this.state.currentTab === "About Us" ? (
+            <AboutUs />
+          ) : (
+            <h1 align="center">"Page not available"</h1>
+          )}
         </div>
-      );
-    } else {
-      return (
-        <div>
-          <Navbar action={this.changeTab} />
-          <div>
-            {this.state.currentTab === "Dashboard" ? (
-              <Dashboard />
-            ) : this.state.currentTab === "Create TimeTable" ? (
-              <CreateTimeTable />
-            ) : this.state.currentTab === "Share TimeTable" ? (
-              <ShareTimeTable />
-            ) : this.state.currentTab === "About Us" ? (
-              <AboutUs />
-            ) : this.state.currentTab === "Logout" ? (
-              <Landing></Landing>
-            ) : (
-              <h1 align='center'>"Page not available"</h1>
-            )}
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
