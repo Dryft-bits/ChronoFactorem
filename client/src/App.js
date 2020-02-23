1046d5febf28234e89ce5f43ef5bfaaf01534aaa
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-02-23 11:05:03 +0530

    Make CreateTimeTable page as default page

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Dashboard from "./components/Dashboard"
-import CreateTimeTable from "./components/CreateTimeTable"
-import ShareTimeTable from "./components/ShareTimeTable"
-import AboutUs from "./components/AboutUs"
+import Dashboard from "./components/Dashboard";
+import CreateTimeTable from "./components/CreateTimeTable";
+import ShareTimeTable from "./components/ShareTimeTable";
+import AboutUs from "./components/AboutUs";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentTab: "Dashboard" }
-    this.changeTab=this.changeTab.bind(this);
+    this.state = { currentTab: "Create TimeTable" };
+    this.changeTab = this.changeTab.bind(this);
   }
-  changeTab(input)
-  {
+  changeTab(input) {
     console.log(input.target.text);
-    let selectedTab=input.target.text;
+    let selectedTab = input.target.text;
     this.setState({
       currentTab: selectedTab
-    })
+    });
   }
-  render() { 
-    return ( <div>
-      <Navbar action={this.changeTab}/>
+  render() {
+    return (
       <div>
-        {this.state.currentTab==="Dashboard"?
-          <Dashboard />
-          :this.state.currentTab==="Create TimeTable" ?
-          <CreateTimeTable />
-          :this.state.currentTab==="Share TimeTable" ?
-          <ShareTimeTable />
-          :this.state.currentTab==="About Us"?
-          <AboutUs />:<h1 align="center">"Page not available"</h1>
-        }
+        <Navbar action={this.changeTab} />
+        <div>
+          {this.state.currentTab === "Dashboard" ? (
+            <Dashboard />
+          ) : this.state.currentTab === "Create TimeTable" ? (
+            <CreateTimeTable />
+          ) : this.state.currentTab === "Share TimeTable" ? (
+            <ShareTimeTable />
+          ) : this.state.currentTab === "About Us" ? (
+            <AboutUs />
+          ) : (
+            <h1 align="center">"Page not available"</h1>
+          )}
+        </div>
       </div>
-    </div> );
+    );
   }
 }
- 
+
 export default App;
