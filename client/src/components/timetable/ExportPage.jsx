390a481bd4a9f01ea2929cc60c53cc49201665ff
Kushagra Gupta <kushgpt08@gmail.com>
2020-03-27 19:16:32 +0530

    Not working!!

--- client/src/components/timetable/ExportPage.jsx
+++ client/src/components/timetable/ExportPage.jsx
@@ -6,6 +6,7 @@ import MidsemSched from "./MidsemSched.jsx";
 import CompreSched from "./CompreSched.jsx";
 
 import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 class ExportPage extends Component {
   constructor(props) {
@@ -13,31 +14,49 @@ class ExportPage extends Component {
   }
 
   componentDidMount() {
-    const pageToExport = document.getElementById("export-page");
-    html2canvas(pageToExport)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
-        window.open(imgData, "_blank");
-        document.getElementById('image').href = imgData;
-        document.getElementById('image').download = 'time.png';
-        document.getElementById('image').click()
-      }).then(() => {
-        document.getElementById('0').click();
-      });
+    const timetableComp = document.getElementById("timetable-pdf");
+    const pageHeight = timetableComp.offsetHeight;
+    const pageWidth = timetableComp.offsetWidth;
+    const midsemComp = document.getElementById("midsem-pdf");
+    const compreComp = document.getElementById("compre-pdf");
+    const pdf = new jsPDF('landscape', 'px', [pageWidth, pageHeight]);
+    const a = html2canvas(timetableComp);
+    const b = html2canvas(midsemComp);
+    const c = html2canvas(compreComp);
+    a.then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      pdf.addImage(imgData, 'PNG', 5, 8);
+      // }).then(() => {
+      //   b.then((canvas) => {
+      //     const imgData = canvas.toDataURL('image/png');
+      //     pdf.addImage(imgData, 'PNG', 5, 8);
+      //   })
+      // }).then(() => {
+      //   c.then((canvas) => {
+      //     const imgData = canvas.toDataURL('image/png');
+      //     pdf.addImage(imgData, 'PNG', 5, 8);
+      //   })
+    }).then(() => {
+      pdf.save('timetable.pdf');
+    }).then(() => {
+      document.getElementById('0').click();
+    });
   }
 
   render() {
-    return <>
-      <div id="export-page">
-        <div id="myMm" style={{ height: "1mm" }} />
+    return (<div id="export-page">
+      <div id="timetable-pdf" >
         <PreviewTT />
-        <h3>MidSem</h3>
-        <MidsemSched />
-        <h3>Compre</h3>
-        <CompreSched />
       </div>
-      <a id="image"></a>
-    </>
+      <div id="midsem-pdf">
+        <h3>MidSem</h3>
+        <MidsemSched id="midsem-pdf" />
+      </div>
+      <div id="compre-pdf">
+        <h3>Compre</h3>
+        <CompreSched id="compre-pdf" />
+      </div>
+    </div>)
   }
 }
 
