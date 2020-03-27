9578e1e1c0f325b522fcd5c779b9ad1898e22c65
Kushagra Gupta <kushgpt08@gmail.com>
2020-03-27 21:42:21 +0530

    Somwhat working pdf export

--- client/src/components/timetable/ExportPage.jsx
+++ client/src/components/timetable/ExportPage.jsx
@@ -18,29 +18,27 @@ class ExportPage extends Component {
     const pageHeight = timetableComp.offsetHeight;
     const pageWidth = timetableComp.offsetWidth;
     const midsemComp = document.getElementById("midsem-pdf");
+    const secondPageOffset = midsemComp.offsetHeight;
     const compreComp = document.getElementById("compre-pdf");
-    const pdf = new jsPDF('landscape', 'px', [pageWidth, pageHeight]);
+    const pdf = new jsPDF('landscape', 'px',
+      [pageHeight, pageWidth]
+    );
     const a = html2canvas(timetableComp);
     const b = html2canvas(midsemComp);
     const c = html2canvas(compreComp);
-    a.then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      pdf.addImage(imgData, 'PNG', 5, 8);
-      // }).then(() => {
-      //   b.then((canvas) => {
-      //     const imgData = canvas.toDataURL('image/png');
-      //     pdf.addImage(imgData, 'PNG', 5, 8);
-      //   })
-      // }).then(() => {
-      //   c.then((canvas) => {
-      //     const imgData = canvas.toDataURL('image/png');
-      //     pdf.addImage(imgData, 'PNG', 5, 8);
-      //   })
-    }).then(() => {
+    Promise.all([a, b, c]).then((canvases) => {
+      const imgData1 = canvases[0].toDataURL('image/png');
+      console.log(imgData1);
+      const imgData2 = canvases[1].toDataURL('image/png');
+      const imgData3 = canvases[2].toDataURL('image/png');
+      pdf.addImage(imgData1, 'PNG', 0, 0);
+      pdf.addPage();
+      pdf.addImage(imgData2, 'PNG', 64, 32);
+      pdf.addImage(imgData3, 'PNG', 64, 64 + secondPageOffset);
       pdf.save('timetable.pdf');
     }).then(() => {
       document.getElementById('0').click();
-    });
+    })
   }
 
   render() {
@@ -49,11 +47,11 @@ class ExportPage extends Component {
         <PreviewTT />
       </div>
       <div id="midsem-pdf">
-        <h3>MidSem</h3>
+        <h3 style={{ textAlign: "center" }}>MidSem</h3>
         <MidsemSched id="midsem-pdf" />
       </div>
-      <div id="compre-pdf">
-        <h3>Compre</h3>
+      <div id="compre-pdf" style={{ textAlign: "center" }}>
+        <h3 >Compre</h3>
         <CompreSched id="compre-pdf" />
       </div>
     </div>)
