a3cf09f2fc5eb077fb75b76ac3873adfa9953d92
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2020-03-27 17:58:26 +0530

    Revert "Click to remove"

--- client/src/schemas/Entry.js
+++ client/src/schemas/Entry.js
@@ -1,10 +1,9 @@
 class Entry {
-  constructor(code, name, room,len,section) {
+  constructor(code, name, room,len) {
     this.courseCode = code;
     this.courseName = name;
     this.sectionRoom = room;
     this.numHours = len;
-    this.section = section;
   }
 }
 
