9559c19dc6f9197fa879514cf6a5461340e8ea78
Nighthawx <41290641+VikramjeetD@users.noreply.github.com>
2020-04-03 20:51:51 +0530

    Close server on testing end

--- test/routes/api/helform.js
+++ test/routes/api/helform.js
@@ -8,9 +8,9 @@ var server = require("../../../server.js");
 
 chai.use(chaiHttp);
 
-describe("Helform api tests", function () {
-  it("/submit allows correct requests", async function () {
-    server.request.isAuthenticated = function () {
+describe("Helform api tests", function() {
+  it("/submit allows correct requests", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -25,14 +25,14 @@ describe("Helform api tests", function () {
       .request(server)
       .post("/api/helform/submit")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(201);
         expect(res.body).to.eql({ msg: "Submitted!" });
       });
   });
 
-  it("/submit does not allow non-numerical slot number", async function () {
-    server.request.isAuthenticated = function () {
+  it("/submit does not allow non-numerical slot number", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -41,19 +41,21 @@ describe("Helform api tests", function () {
       branch: ["your head", "my foot"],
       year: 1
     };
-    submissionData = JSON.parse('{"slotNumber": "45edfd", "humanitiesElectives": []}');
+    submissionData = JSON.parse(
+      '{"slotNumber": "45edfd", "humanitiesElectives": []}'
+    );
     server.request.user = testUser;
     chai
       .request(server)
       .post("/api/helform/submit")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(422);
       });
   });
 
-  it("/submit does not allow non-array humanities electives", async function () {
-    server.request.isAuthenticated = function () {
+  it("/submit does not allow non-array humanities electives", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -62,19 +64,21 @@ describe("Helform api tests", function () {
       branch: ["your head", "my foot"],
       year: 1
     };
-    submissionData = JSON.parse('{"slotNumber": 4, "humanitiesElectives": "fjdfbdf"}');
+    submissionData = JSON.parse(
+      '{"slotNumber": 4, "humanitiesElectives": "fjdfbdf"}'
+    );
     server.request.user = testUser;
     chai
       .request(server)
       .post("/api/helform/submit")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(422);
       });
   });
 
-  it("/firstlogin works for correct request", async function () {
-    server.request.isAuthenticated = function () {
+  it("/firstlogin works for correct request", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -83,19 +87,21 @@ describe("Helform api tests", function () {
       branch: ["your head", "my foot"],
       year: 1
     };
-    submissionData = JSON.parse('{"email": "foo.bar@baz.com", "studentBranch": ["CS"], "year": 2020}');
+    submissionData = JSON.parse(
+      '{"email": "foo.bar@baz.com", "studentBranch": ["CS"], "year": 2020}'
+    );
     server.request.user = testUser;
     chai
       .request(server)
       .post("/api/helform/firstlogin")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(201);
       });
   });
 
-  it("/firstlogin does not allow malformed emails", async function () {
-    server.request.isAuthenticated = function () {
+  it("/firstlogin does not allow malformed emails", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -104,19 +110,21 @@ describe("Helform api tests", function () {
       branch: ["your head", "my foot"],
       year: 1
     };
-    submissionData = JSON.parse('{"email": "foo.bar@", "studentBranch": ["CS"], "year": 2020}');
+    submissionData = JSON.parse(
+      '{"email": "foo.bar@", "studentBranch": ["CS"], "year": 2020}'
+    );
     server.request.user = testUser;
     chai
       .request(server)
       .post("/api/helform/firstlogin")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(422);
       });
   });
 
-  it("/firstlogin does not allow non array branches", async function () {
-    server.request.isAuthenticated = function () {
+  it("/firstlogin does not allow non array branches", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -125,19 +133,21 @@ describe("Helform api tests", function () {
       branch: ["your head", "my foot"],
       year: 1
     };
-    submissionData = JSON.parse('{"email": "foo.bar@baz.com", "studentBranch": "CS", "year": 2020}');
+    submissionData = JSON.parse(
+      '{"email": "foo.bar@baz.com", "studentBranch": "CS", "year": 2020}'
+    );
     server.request.user = testUser;
     chai
       .request(server)
       .post("/api/helform/firstlogin")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(422);
       });
   });
 
-  it("/firstlogin does not allow non numerical years", async function () {
-    server.request.isAuthenticated = function () {
+  it("/firstlogin does not allow non numerical years", async function() {
+    server.request.isAuthenticated = function() {
       return true;
     };
     testUser = {
@@ -146,19 +156,16 @@ describe("Helform api tests", function () {
       branch: ["your head", "my foot"],
       year: 1
     };
-    submissionData = JSON.parse('{"email": "foo.bar@baz.com", "studentBranch": ["CS"], "year": "abcd"}');
+    submissionData = JSON.parse(
+      '{"email": "foo.bar@baz.com", "studentBranch": ["CS"], "year": "abcd"}'
+    );
     server.request.user = testUser;
     chai
       .request(server)
       .post("/api/helform/firstlogin")
       .send(submissionData)
-      .end(function (err, res) {
+      .end(function(err, res) {
         expect(res.status).to.be.equal(422);
       });
-  });
-
-  // After all tests are stop the server
-  after(async () => {
-    server.stop();
   });
 });
