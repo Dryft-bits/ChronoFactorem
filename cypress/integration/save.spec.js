8bf4055b1447916a5b93dc725744e321cbfe7b35
Kushagra Gupta <kushgpt08@gmail.com>
2020-04-06 17:52:35 +0530

    Add more tests for save timetable

--- cypress/integration/save.spec.js
+++ cypress/integration/save.spec.js
@@ -16,6 +16,19 @@ context('Save TimeTable', () => {
     cy.get('.gridItem').should('have.length', 1);
   })
 
+  it('should alert on course clash', () => {
+    cy.get('[id="BIO F110"]').click();
+    cy.get('h5[id="L1"]').click();
+    cy.get('button[name="Change Course"]').click();
+    cy.get('[id="CS F212"]').click();
+    cy.get('h5[id="L1"]').click();
+    cy.get('.gridItem').should('have.length', 1);
+  })
+
+  it('empty timetable should save', () => {
+    cy.contains('Save TimeTable');
+  })
+
   it('timetable should save', () => {
     cy.get('[id="BIO F110"]').click();
     cy.get('h5[id="L1"]').click();
