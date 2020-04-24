eae0a8e9ff66d91a27b1234092d9343ca3d3d152
VikramjeetD <41290641+VikramjeetD@users.noreply.github.com>
2020-04-24 12:57:19 +0530

    Make changes for dashboard redirect

--- client/src/components/tabs/Landing.jsx
+++ client/src/components/tabs/Landing.jsx
@@ -4,20 +4,19 @@ import configuration from "../../config/constants.js";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { useState } from "react";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { addProf } from '../../redux/actions/auth';
-import CreateAccount from '../forms/CreateAccount';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { addProf } from "../../redux/actions/auth";
+import CreateAccount from "../forms/CreateAccount";
 
 export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
-
   let token = Cookies.get("token") ? Cookies.get("token") : null;
 
   addProf(token);
@@ -28,15 +27,22 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
     password: "",
     incorrectUsername: false,
     incorrectPassword: false,
-    isCreate: false,
+    isCreate: false
   });
-  const { isOpen, username, password, incorrectUsername, incorrectPassword, isCreate } = open;
+  const {
+    isOpen,
+    username,
+    password,
+    incorrectUsername,
+    incorrectPassword,
+    isCreate
+  } = open;
   const handleCreate = () => {
     setOpen({
       ...open,
       isCreate: !isCreate
     });
-  }
+  };
   const handleClickOpen = () => {
     setOpen({ ...open, isOpen: true });
   };
@@ -45,48 +51,52 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
     setOpen({ ...open, isOpen: false });
   };
 
-  const editUsername = (e) => {
+  const editUsername = e => {
     e.preventDefault();
     setOpen({
       ...open,
       username: e.target.value,
       incorrectUsername: false
     });
-  }
-  const editPassword = (e) => {
+  };
+  const editPassword = e => {
     e.preventDefault();
     setOpen({
       ...open,
       password: e.target.value,
-      incorrectPassword: false,
+      incorrectPassword: false
     });
-  }
+  };
   const config = {
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-
+      "Access-Control-Allow-Origin": "*"
     }
   };
-  const submit = async (e) => {
-    let iu = false, ip = false;
+  const submit = async e => {
+    let iu = false,
+      ip = false;
     e.preventDefault();
-    await axios.post("/api/ProfAuth",
-      {
-        username: username,
-        password: password
-      }, config).then(function (res) {
-
+    await axios
+      .post(
+        "/api/ProfAuth",
+        {
+          username: username,
+          password: password
+        },
+        config
+      )
+      .then(function (res) {
         if (!res || res.status === 204) {
           iu = true;
-        }
-        else {
-          iu = false
-          if (res.status === 206) { ip = true; }
-          else {
+        } else {
+          iu = false;
+          if (res.status === 206) {
+            ip = true;
+          } else {
             ip = false;
             //store the token in HTTP cookie
-            Cookies.set('token', res.data.token, { expires: 1 });
+            Cookies.set("token", res.data.token, { expires: 1 });
             window.alert("welcome");
             addProf(res.data.token);
           }
@@ -94,11 +104,13 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
         setOpen({
           ...open,
           incorrectUsername: iu,
-          incorrectPassword: ip,
+          incorrectPassword: ip
         });
-      }
-      ).catch((err) => { console.log('Axios Error:', err); });
-  }
+      })
+      .catch(err => {
+        console.log("Axios Error:", err);
+      });
+  };
 
   if (profAuthenticated) {
     //redirect to dash here
@@ -108,11 +120,7 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
     return <Redirect to='/checkloggedin'></Redirect>;
   }
 
-
-
-
   return (
-
     <section className='landing body'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
@@ -122,9 +130,11 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
               Create your own timetable.
             </p>
             <div>
-              <button className='btn-landing btn-left' onClick={handleClickOpen}>
-                <span>Staff </span>
-              </button>
+              <a href={configuration.urls.adminLogin}>
+                <button className='btn-landing btn-left'>
+                  <span>Staff </span>
+                </button>
+              </a>
               <a href={configuration.urls.googleAuth}>
                 <button className='btn-landing btn-right'>
                   <span>Student </span>
@@ -135,20 +145,24 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
         </div>
       </div>
       <div>
-        <Dialog open={isOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <Dialog
+          open={isOpen}
+          onClose={handleClose}
+          aria-labelledby='form-dialog-title'
+        >
           <form onSubmit={submit}>
-            <DialogTitle id="form-dialog-title">Login</DialogTitle>
+            <DialogTitle id='form-dialog-title'>Login</DialogTitle>
             <DialogContent>
               <DialogContentText>
                 Enter your username and password
-          </DialogContentText>
+              </DialogContentText>
               <TextField
                 autoFocus
                 error={incorrectUsername}
-                margin="dense"
-                id="name"
-                label="Username"
-                type="text"
+                margin='dense'
+                id='name'
+                label='Username'
+                type='text'
                 fullWidth
                 onChange={editUsername}
                 helperText={incorrectUsername ? "Username does not exist" : ""}
@@ -156,33 +170,37 @@ export const Landing = ({ isAuthenticated, profAuthenticated, addProf }) => {
               <TextField
                 error={incorrectPassword}
                 autoFocus
-                margin="dense"
-                id="name"
-                label="Password"
-                type="password"
+                margin='dense'
+                id='name'
+                label='Password'
+                type='password'
                 fullWidth
                 onChange={editPassword}
-                helpertext={incorrectPassword ? "Incorrect Password for given username" : ""}
+                helpertext={
+                  incorrectPassword
+                    ? "Incorrect Password for given username"
+                    : ""
+                }
               />
             </DialogContent>
 
             <DialogActions>
-              <Button onClick={handleClose} color="primary">
+              <Button onClick={handleClose} color='primary'>
                 Cancel
-          </Button>
-              <Button type='submit' color="primary">
+              </Button>
+              <Button type='submit' color='primary'>
                 Login
-          </Button>
-              <Button onClick={handleCreate} color="primary">
+              </Button>
+              <Button onClick={handleCreate} color='primary'>
                 Create new
-          </Button>
+              </Button>
             </DialogActions>
           </form>
         </Dialog>
         <CreateAccount open={isCreate} action={handleCreate} />
       </div>
-    </section>);
-
+    </section>
+  );
 };
 
 const mapStateToProps = state => {
@@ -191,7 +209,5 @@ const mapStateToProps = state => {
     profAuthenticated: state.auth.profAuthenticated
   };
 };
-
-
 
 export default connect(mapStateToProps, { addProf })(Landing);
