541ca9009d35420144c7919cb809ea99970f06cd
VikramjeetD <41290641+VikramjeetD@users.noreply.github.com>
2020-05-06 19:48:42 +0530

    Add admin dashboard link

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,7 +1,7 @@
 const prod = {
   urls: {
     googleAuth: "https://chronofactorem.herokuapp.com/api/auth/google",
-    adminLogin: ""
+    adminLogin: "https://chrono-dashboard.herokuapp.com/#/login"
   }
 };
 
