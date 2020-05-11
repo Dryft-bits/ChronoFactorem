27e4ee91902767b5c025ff69267392beb865cccc
Aviral14 <aviralagarwal25@gmail.com>
2020-05-11 18:40:14 +0530

    Replace window alerts with material ui alerts

--- client/src/components/tabs/Dashboard.jsx
+++ client/src/components/tabs/Dashboard.jsx
@@ -6,78 +6,107 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { editTT } from "../../redux/actions/UpdateTimeTable";
 import ShareTimeTable from "./ShareTimeTable";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+import AlertBox from "../utils/AlertBox.jsx";
 import "../../styles/Dashboard.css";
+
 const useStyles = makeStyles({
   root: {
     minWidth: 250,
-    margin:"1vh"
+    margin: "1vh",
   },
   cardcontainer: {
     width: "100%",
     maxWidth: "95vw",
     maxHeight: "30vh",
-    overflow: "auto"    
+    overflow: "auto",
   },
   grid: {
     flexGrow: 1,
     maxWidth: "95vw",
     maxHeight: "26vh",
-    overflow: "auto"
-  }
+    overflow: "auto",
+  },
 });
-const Dashboard = props => {
+const Dashboard = (props) => {
   const classes = useStyles();
   const [TTData, setTTData] = React.useState({
-    savedTT: null
+    savedTT: null,
+    AlertMsg: { status: false, type: null, msg: null },
   });
-  const { savedTT } = TTData;
+  const { savedTT, AlertMsg } = TTData;
 
   function deleteTT(id) {
+    let newMsg;
     try {
-      axios.delete(`/api/timetable/delete/${id}`).then(res => {
+      axios.delete(`/api/timetable/delete/${id}`).then((res) => {
         if (res.status === 200) {
-          window.alert("Successfully Deleted the Timetable");
+          let newData = (savedTT || data).filter((item) => item._id !== id);
+          newMsg = {
+            status: true,
+            type: "success",
+            msg: "Successfully Deleted the Timetable",
+          };
+          setTTData({ savedTT: newData, AlertMsg: newMsg });
         } else {
-          window.alert(res.data.msg);
+          newMsg = {
+            status: true,
+            type: "error",
+            msg: res.data.msg + " Please Try Again Later",
+          };
+          setTTData({ ...TTData, AlertMsg: newMsg });
         }
-        let newData = (TTData.savedTT || data).filter(item => item._id !== id);
-        setTTData({ savedTT: newData });
       });
     } catch (err) {
-      window.alert("Couldn't Delete The TimeTable! Please Try Again Later.");
+      newMsg = {
+        status: true,
+        type: "error",
+        msg: "Couldn't Delete The TimeTable! Please Try Again Later.",
+      };
+      setTTData({ ...TTData, AlertMsg: newMsg });
     }
   }
 
   function toggleShare(id, action) {
+    let newMsg;
     try {
-      axios.get(`/api/timetable/toggleShare/${id}`).then(res => {
+      axios.get(`/api/timetable/toggleShare/${id}`).then((res) => {
         if (res.status === 200) {
-          window.alert("Successfully " + action + " the Timetable");
-        } else {
-          window.alert(res.data.msg);
-        }
-        let newData = [];
-        for (let item of TTData.savedTT || data) {
-          if (item._id === id) {
-            item.isShared = !item.isShared;
+          let newData = [];
+          for (let item of savedTT || data) {
+            if (item._id === id) {
+              item.isShared = !item.isShared;
+            }
+            newData.push(item);
           }
-          newData.push(item);
+          newMsg = {
+            status: true,
+            type: "success",
+            msg: "Successfully " + action + " the Timetable",
+          };
+          setTTData({ savedTT: newData, AlertMsg: newMsg });
+        } else {
+          newMsg = {
+            status: true,
+            type: "error",
+            msg: res.data.msg,
+          };
+          setTTData({ ...TTData, AlertMsg: newMsg });
         }
-        setTTData({ savedTT: newData });
       });
     } catch (err) {
-      window.alert(
-        "The TimeTable Couldn't be " + action + "! Please Try Again Later."
-      );
+      newMsg = {
+        status: true,
+        type: "error",
+        msg: "res.data.msg" + " Please Try Again Later",
+      };
+      setTTData({ ...TTData, AlertMsg: newMsg });
     }
   }
   let [data, loading] = useGetData("/api/timetable/getTT");
@@ -85,88 +114,98 @@ const Dashboard = props => {
   if (!loading) {
     return (
       <>
-        <h4 className='title'>Saved Timetables</h4>
-            <div className={classes.grid} style={{position:"relative",left:"2vw" }}>
-              <Grid container style={{ backgroundColor:"#74b9ff"}}>
-            {(TTData.savedTT || data).map(itemc => {
+        <AlertBox
+          AlertMsg={AlertMsg}
+          handleClose={() => {
+            setTTData({
+              ...TTData,
+              AlertMsg: { status: false, type: null, msg: null },
+            });
+          }}
+        />
+        <h4 className="title">Saved Timetables</h4>
+        <h4 className="title">Saved Timetables</h4>
+        <div
+          className={classes.grid}
+          style={{ position: "relative", left: "2vw" }}
+        >
+          <Grid container style={{ backgroundColor: "#74b9ff" }}>
+            {(TTData.savedTT || data).map((itemc) => {
               return (
                 <>
                   <Grid item xs={6}>
-                  <div
-                    key={itemc._id}
-                    id={itemc._id}
-                  >
-                  <Card className={classes.root}>
-                    <CardContent>
-                      <Typography
-                        className={classes.root}
-                        color='textSecondary'
-                        gutterBottom
-                      >
-                        Name
-                      </Typography>
-                      <Typography variant='h5' component='h2'>
-                        {itemc.name}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Link
-                        to='/create'
-                        onClick={() => {
-                          props.editTT(itemc);
-                        }}
-                      >
-                        <Button
-                          variant='contained'
-                          color='primary'
-                          size='small'
-                        >
-                          Edit
-                        </Button>
-                      </Link>
+                    <div key={itemc._id} id={itemc._id}>
+                      <Card className={classes.root}>
+                        <CardContent>
+                          <Typography
+                            className={classes.root}
+                            color="textSecondary"
+                            gutterBottom
+                          >
+                            Name
+                          </Typography>
+                          <Typography variant="h5" component="h2">
+                            {itemc.name}
+                          </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <Link
+                            to="/create"
+                            onClick={() => {
+                              props.editTT(itemc);
+                            }}
+                          >
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              size="small"
+                            >
+                              Edit
+                            </Button>
+                          </Link>
 
-                      <Button
-                        onClick={() => {
-                          deleteTT(itemc._id);
-                        }}
-                        variant='contained'
-                        color='secondary'
-                        size='small'
-                      >
-                        Delete
-                      </Button>
-                      {itemc.isShared ? (
-                        <Button
-                          variant='contained'
-                          size='small'
-                          onClick={() => {
-                            toggleShare(itemc._id, "Unshared");
-                          }}
-                        >
-                          Unshare
-                        </Button>
-                      ) : (
-                        <Button
-                          variant='contained'
-                          size='small'
-                          onClick={() => {
-                            toggleShare(itemc._id, "Shared");
-                          }}
-                        >
-                          Share
-                        </Button>
-                      )}
-                    </CardActions>
-                  </Card>
-                  </div>
+                          <Button
+                            onClick={() => {
+                              deleteTT(itemc._id);
+                            }}
+                            variant="contained"
+                            color="secondary"
+                            size="small"
+                          >
+                            Delete
+                          </Button>
+                          {itemc.isShared ? (
+                            <Button
+                              variant="contained"
+                              size="small"
+                              onClick={() => {
+                                toggleShare(itemc._id, "Unshared");
+                              }}
+                            >
+                              Unshare
+                            </Button>
+                          ) : (
+                            <Button
+                              variant="contained"
+                              size="small"
+                              onClick={() => {
+                                toggleShare(itemc._id, "Shared");
+                              }}
+                            >
+                              Share
+                            </Button>
+                          )}
+                        </CardActions>
+                      </Card>
+                    </div>
                   </Grid>
                 </>
               );
             })}
-            </Grid>
-          </div>
+          </Grid>
+        </div>
 
-        <h4 className='title'>Publicily Shared Timetables</h4>
+        <h4 className="title">Publicily Shared Timetables</h4>
         <ShareTimeTable />
       </>
     );
@@ -175,14 +214,14 @@ const Dashboard = props => {
   }
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    editTT: tt => dispatch(editTT(tt))
+    editTT: (tt) => dispatch(editTT(tt)),
   };
 };
 
 Dashboard.propTypes = {
-  editTT: PropTypes.func.isRequired
+  editTT: PropTypes.func.isRequired,
 };
 
 export default connect(null, mapDispatchToProps)(Dashboard);
