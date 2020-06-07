1176a1fde6bbbb27499a7c53af38eb4fa47787bf
Aviral14 <aviralagarwal25@gmail.com>
2020-06-07 20:24:14 +0530

    Refactor code to record no of tts

--- server/utils/statsCalculator.js
+++ server/utils/statsCalculator.js
@@ -2,30 +2,39 @@ const CourseStats = require("../models/CourseStats");
 const Student = require("../models/Student");
 
 const updateOnSaving = async (studentId, courses) => {
-  let studentObj = await Student.findOne({ _id: studentId });
-  for (let chosen of courses) {
-    let flag = 0;
-    let id = Object.keys(chosen.course)[0];
-    for (let taken of studentObj.interestedCourses) {
-      if (id === taken) {
-        flag = 1;
-        break;
+  try {
+    let i;
+    let studentObj = await Student.findOne({ _id: studentId });
+    let toSearch = studentObj.interestedCourses;
+    for (let chosen of courses) {
+      let courseId = Object.keys(chosen.course)[0];
+      for (i = 0; i < toSearch.length; i++) {
+        if (courseId === Object.keys(toSearch[i])[0]) {
+          break;
+        }
       }
-    }
-    if (!flag) {
-      studentObj.interestedCourses.push(id);
-      let stats = await CourseStats.findOne({ courseId: id });
-      if (!stats) {
-        stats = new CourseStats({
-          courseId: id,
-          count: 1,
-        });
+      let obj = {};
+      if (i < toSearch.length) {
+        obj[courseId] = studentObj.interestedCourses[i][courseId] + 1;
+        studentObj.interestedCourses.set(i, obj);
       } else {
-        stats.count++;
+        obj[courseId] = 1;
+        studentObj.interestedCourses.push(obj);
+        let stats = await CourseStats.findOne({ courseId: courseId });
+        if (!stats) {
+          stats = new CourseStats({
+            courseId: courseId,
+            count: 1,
+          });
+        } else {
+          stats.count++;
+        }
+        await stats.save();
       }
-      stats.save();
-      studentObj.save();
+      await studentObj.save();
     }
+  } catch (err) {
+    throw err;
   }
 };
 const updateOnDeleting = async () => {};
