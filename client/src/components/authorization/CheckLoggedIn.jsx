1a796986acaa761292c5e2890da5e468b3f34223
VikramjeetD <41290641+VikramjeetD@users.noreply.github.com>
2020-06-13 18:20:27 +0530

    Redirect fixes;

--- client/src/components/authorization/CheckLoggedIn.jsx
+++ client/src/components/authorization/CheckLoggedIn.jsx
@@ -3,38 +3,38 @@ import { Redirect } from "react-router-dom";
 import { useGetData } from "use-axios-react";
 import { connect } from "react-redux";
 import { verifyLogin } from "../../redux/actions/auth";
-import { makeStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const CheckLoggedIn = ({ verifyLogin }) => {
   useEffect(() => {
     verifyLogin();
   }, [verifyLogin]);
   const [userInfo, loading] = useGetData("/api/current_user");
-  const useStyles = makeStyles((theme) => ({
+  const useStyles = makeStyles(theme => ({
     root: {
-      width: '100%',
-      '& > * + *': {
-        marginTop: theme.spacing(2),
-      },
-    },
+      width: "100%",
+      "& > * + *": {
+        marginTop: theme.spacing(2)
+      }
+    }
   }));
 
-  const useStylesCircular = makeStyles((theme) => ({
+  const useStylesCircular = makeStyles(theme => ({
     root: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      '& > * + *': {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      "& > * + *": {
         marginLeft: theme.spacing(2),
-        marginTop: theme.spacing(2),
-      },
-    },
+        marginTop: theme.spacing(2)
+      }
+    }
   }));
   const Cclasses = useStylesCircular();
   const classes = useStyles();
-  if (loading) {
+  if (loading || (localStorage.getItem("loggedIn") && !userInfo)) {
     return (
       <>
         <div className={classes.root}>
@@ -43,7 +43,8 @@ const CheckLoggedIn = ({ verifyLogin }) => {
         <div className={Cclasses.root}>
           <CircularProgress />
         </div>
-      </>/*<Redirect to='/checkloggedin'></Redirect>*/);
+      </> /*<Redirect to='/checkloggedin'></Redirect>*/
+    );
   } else if (!userInfo) {
     return <Redirect to='/'></Redirect>;
   } else {
--- client/src/components/routes/PrivateRoute.jsx
+++ client/src/components/routes/PrivateRoute.jsx
@@ -4,6 +4,9 @@ import { useGetData } from "use-axios-react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { verifyLogin } from "../../redux/actions/auth";
+import { makeStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const PrivateRoute = ({
   component: Component,
@@ -13,7 +16,28 @@ const PrivateRoute = ({
   ...rest
 }) => {
   const [userInfo, loading] = useGetData("/api/current_user");
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: "100%",
+      "& > * + *": {
+        marginTop: theme.spacing(2)
+      }
+    }
+  }));
 
+  const useStylesCircular = makeStyles(theme => ({
+    root: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      "& > * + *": {
+        marginLeft: theme.spacing(2),
+        marginTop: theme.spacing(2)
+      }
+    }
+  }));
+  const Cclasses = useStylesCircular();
+  const classes = useStyles();
   return (
     <Route
       {...rest}
@@ -21,8 +45,14 @@ const PrivateRoute = ({
         !localStorage.getItem("loggedIn") ? (
           <Redirect to='/'></Redirect>
         ) : loading ? (
-          // Put a cool animation here
-          <div>Loading...</div>
+          <>
+            <div className={classes.root}>
+              <LinearProgress />
+            </div>
+            <div className={Cclasses.root}>
+              <CircularProgress />
+            </div>
+          </>
         ) : !((!submitted && userInfo.submittedForm) || submitted) ? (
           <Redirect to='/helform'></Redirect>
         ) : (
